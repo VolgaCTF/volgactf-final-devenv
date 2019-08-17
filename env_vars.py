653fe0ea73fe2c80d121ca052b0ede12ac4d0e34
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-08-17 20:01:19 +0400

    Update dependencies

--- env_vars.py
+++ env_vars.py
@@ -15,10 +15,10 @@ def main():
         master_pwd = get_random_str()
         checker_pwd = get_random_str()
         template1 = (
-            'THEMIS_FINALS_AUTH_MASTER_USERNAME={0}\n'
-            'THEMIS_FINALS_AUTH_MASTER_PASSWORD={1}\n'
-            'THEMIS_FINALS_AUTH_CHECKER_USERNAME={2}\n'
-            'THEMIS_FINALS_AUTH_CHECKER_PASSWORD={3}'
+            'VOLGACTF_FINAL_AUTH_MASTER_USERNAME={0}\n'
+            'VOLGACTF_FINAL_AUTH_MASTER_PASSWORD={1}\n'
+            'VOLGACTF_FINAL_AUTH_CHECKER_USERNAME={2}\n'
+            'VOLGACTF_FINAL_AUTH_CHECKER_PASSWORD={3}'
         ).format(
             'master',
             master_pwd,
@@ -32,9 +32,9 @@ def main():
 
     with open('volgactf-final-public.env', 'w', encoding='utf-8') as f2:
         template2 = (
-            'THEMIS_FINALS_FLAG_SIGN_KEY_PUBLIC={0}\n'
-            'THEMIS_FINALS_FLAG_WRAP_PREFIX={1}\n'
-            'THEMIS_FINALS_FLAG_WRAP_SUFFIX={2}'
+            'VOLGACTF_FINAL_FLAG_SIGN_KEY_PUBLIC={0}\n'
+            'VOLGACTF_FINAL_FLAG_WRAP_PREFIX={1}\n'
+            'VOLGACTF_FINAL_FLAG_WRAP_SUFFIX={2}'
         ).format(
             public_key.to_pem().decode('ascii').strip().replace('\n', '\\n'),
             'VolgaCTF{',
@@ -46,8 +46,8 @@ def main():
 
     with open('volgactf-final-private.env', 'w', encoding='utf-8') as f3:
         template3 = (
-            'THEMIS_FINALS_FLAG_SIGN_KEY_PRIVATE={0}\n'
-            'THEMIS_FINALS_FLAG_GENERATOR_SECRET={1}'
+            'VOLGACTF_FINAL_FLAG_SIGN_KEY_PRIVATE={0}\n'
+            'VOLGACTF_FINAL_FLAG_GENERATOR_SECRET={1}'
         ).format(
             private_key.to_pem().decode('ascii').strip().replace('\n', '\\n'),
             secret
